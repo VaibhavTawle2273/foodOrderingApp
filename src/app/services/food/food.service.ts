9cdf921f459ce6f401bb2746a0f18ccd0c2f971c
VaibhavTawle2273 <vaibhavtawale2002@gmail.com>
2024-03-30 20:17:39 +0530

    images

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Foods } from '../../shared/models/Food';
 
 @Injectable({
   providedIn: 'root'
@@ -7,15 +8,120 @@ export class FoodService{
 
   constructor() { }
 
-  getAll: string[]=[
-        '/assets/food-1.jpeg',
-        '/assets/food-2.jpeg',
-        '/assets/food-3.jpeg',
-        '/assets/food-4.jpeg',
-        '/assets/food-5.jpeg',
-        '/assets/food-6.jpeg',
-        '/assets/food-7.jpeg',
-        '/assets/food-8.jpeg'
-      ]
-  
+  getAll():Foods[]{
+    return[ 
+      {
+        id: 1,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-1.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:2,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-2.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:3,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-3.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:4,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-4.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:5,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-5.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:6,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-6.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:7,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: false,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-7.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:8,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: true,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-8.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:9,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: true,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-9.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      },
+      {
+        id:10,
+        name: "Pizza pepperoni",
+        price: 9,
+        cookTime: '40-50',
+        favorite: true,
+        origins: ['Italy'],
+        star: 4.0,
+        imageUrl: '/assets/images/foods/food-10.jpeg',
+        tags: ['FastFoos', 'Pizza' , 'Lunch']
+      }
+    ];
+    
+  }
 }
+
